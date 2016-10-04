1b53650f0f7da7561b741837d0147a169e6f629a
hGriff0n <ghooper96@gmail.com>
2016-10-04 15:49:05 -0400

    Added AST pretty printing

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-
+use std::iter;
 
 #[derive(Debug)]
 pub enum LiteralType {
@@ -15,6 +15,7 @@ pub enum BindingType {
     Variable(String),
     Type(String),
     Operator(String),
+    Tuple(Vec<BindingType>),
 }
 
 // Here we see one of the biggest "problems" with Rust, the borrow checker
@@ -37,30 +38,40 @@ pub enum ExprType {
     ModDecl(Vec<BindingType>),
 }
 
+static mut buf_size: usize = 0;
+
+fn mk_buffer(size: usize) -> String {
+    iter::repeat(" ").take(size).collect()
+}
+
 impl fmt::Display for LiteralType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            LiteralType::Num(ref int_part, ref float_part) => {
-                match *float_part {
-                    Some(ref flt) => {
-                        writeln!(f, "Float: {}.{}", int_part, flt)
-                    },
-                    None => {
-                        writeln!(f, "Int: {}", int_part)
+        unsafe {
+            let buff = mk_buffer(buf_size);
+
+            match *self {
+                LiteralType::Num(ref int_part, ref float_part) => {
+                    match *float_part {
+                        Some(ref flt) => {
+                            write!(f, "{}Float: {}.{}", buff, int_part, flt)
+                        },
+                        None => {
+                            write!(f, "{}Int: {}", buff, int_part)
+                        }
                     }
+                },
+                LiteralType::Byte(ref string, is_hex) => {
+                    write!(f, "{}Byte: {}", buff, string)
+                },
+                LiteralType::Bool(val) => {
+                    write!(f, "{}Bool: {}", buff, val)
+                },
+                LiteralType::Str(ref string) => {
+                    write!(f, "{}String: {}", buff, string)
+                },
+                LiteralType::Char(ref ch) => {
+                    write!(f, "{}Char: {}", buff, ch)
                 }
-            },
-            LiteralType::Byte(ref string, is_hex) => {
-                writeln!(f, "Byte: {}", string)
-            },
-            LiteralType::Bool(val) => {
-                writeln!(f, "Bool: {}", val)
-            },
-            LiteralType::Str(ref string) => {
-                writeln!(f, "String: {}", string)
-            },
-            LiteralType::Char(ref ch) => {
-                writeln!(f, "Char: {}", ch)
             }
         }
     }
@@ -68,10 +79,27 @@ impl fmt::Display for LiteralType {
 
 impl fmt::Display for BindingType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            BindingType::Variable(ref var) => write!(f, "Variable: {}", var),
-            BindingType::Type(ref typ) => write!(f, "Type: {}", typ),
-            BindingType::Operator(ref op) => write!(f, "Op: {}", op),
+        unsafe {
+            let buf = mk_buffer(buf_size);
+
+            match *self {
+                BindingType::Variable(ref var) => write!(f, "{}Variable: {}", buf, var),
+                BindingType::Type(ref typ) => write!(f, "{}Type: {}", buf, typ),
+                BindingType::Operator(ref op) => write!(f, "{}Op: {}", buf, op),
+                BindingType::Tuple(ref binds) => {
+                    let o_size = buf_size;
+                    buf_size += 1;
+
+                    write!(f, "{}TupleBinding[{}]", buf, binds.len());
+
+                    for piece in binds {
+                        write!(f, "\n{}", piece);
+                    }
+
+                    buf_size -= 1;
+                    write!(f, "")
+                }
+            }
         }
     }
 }
@@ -90,50 +118,59 @@ impl fmt::Display for Keyword {
 #[allow(unused_variables)]
 impl fmt::Display for ExprType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            ExprType::Assignment(ref key, ref bind, is_mut, ref val) => {
-                writeln!(f, "Binding ({}, {})", key, is_mut);
-                writeln!(f, "{}", bind);
-                write!(f, "{}", val)
-            },
-            ExprType::Literal(ref literal) => {
-                write!(f, "{}", literal)
-            },
-            ExprType::Scope(ref exprs) => {
-                write!(f, "Scope");
+        unsafe {
+            let buf = mk_buffer(buf_size);
+            let o_size = buf_size;
 
-                for expr in exprs {
-                    write!(f, "\n{}", expr);
+            match *self {
+                ExprType::Assignment(ref key, ref bind, is_mut, ref val) => {
+                    writeln!(f, "{}Binding ({}, {})", buf, key, is_mut);
+
+                    buf_size += 1;
+                    writeln!(f, "{}", bind);
+                    write!(f, "{}", val);
+                },
+                ExprType::Literal(ref literal) => {
+                    write!(f, "{}", literal);
+                },
+                ExprType::Scope(ref exprs) => {
+                    write!(f, "{}Scope", buf);
+                    
+                    buf_size += 1;
+                    for expr in exprs {
+                        write!(f, "\n{}", expr);
+                    }
+                },
+                ExprType::Tuple(ref exprs) => {
+                    write!(f, "{}Tuple[{}]", buf, exprs.len());
+
+                    buf_size += 1;
+                    for expr in exprs {
+                        write!(f, "\n{}", expr);
+                    }
+                },
+                ExprType::Binary(ref bind, ref lhs, ref rhs) => {
+                    writeln!(f, "{}Binary Operator {}", buf, bind);
+                    
+                    buf_size += 1;
+                    writeln!(f, "{}", lhs);
+                    write!(f, "{}", rhs);
+                },
+                ExprType::FnCall(ref fun, ref caller, ref args) => {
+                    write!(f, "{}A function call", buf);
+                },
+                ExprType::ModDecl(ref module) => {
+                    write!(f, "{}Module Declaration", buf);
+
+                    buf_size += 1;
+                    for piece in module {
+                        write!(f, "\n{}", piece);
+                    }
                 }
-
-                write!(f, "")
-            },
-            ExprType::Tuple(ref exprs) => {
-                write!(f, "Tuple[{}]", exprs.len());
-
-                for expr in exprs {
-                    write!(f, "\n{}", expr);
-                }
-
-                write!(f, "")
-            },
-            ExprType::Binary(ref bind, ref lhs, ref rhs) => {
-                writeln!(f, "Binary Operator {}", bind);
-                writeln!(f, "{}", lhs);
-                write!(f, "{}", rhs)
-            },
-            ExprType::FnCall(ref fun, ref caller, ref args) => {
-                write!(f, "A function call")
-            },
-            ExprType::ModDecl(ref module) => {
-                write!(f, "Module Declaration");
-
-                for piece in module {
-                    write!(f, "\n{}", piece);
-                }
-
-                write!(f, "")
             }
+
+            buf_size = o_size;
+            write!(f, "")
         }
     }
 }
