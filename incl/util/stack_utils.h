cb7dea1011dcb42f9c38a76d814c99ce6396d8ef
hGriff0n <ghooper96@gmail.com>
2016-10-30 12:41:18 -0400

    Updated grammar to match recent language changes

--- incl/util/stack_utils.h
+++ incl/util/stack_utils.h
@@ -3,34 +3,45 @@
 #include <variant>
 
 namespace spero::util {
-	template<class T, class Iter>
+	/*
+	 * Find the first location of a specific node on the stack
+	 */
+	template<class G, class Iter>
 	Iter findFirst(Iter&& front, Iter&& back) {
 		return std::find_if(front, back, [](auto&& elem) {
-			return std::visit(compose(
-				[](T&) { return true; },
-				[](auto&&) { return false; }
-			), elem);
-		});
-	}
-
-	// Unknown
-	template<class T, class Stack>
-	bool topIs(Stack&& s) {
-		return std::visit(compose(
-			[](T&) { return true; },
-			[](auto&&) { return false; }
-		), s.back());
-	}
-
-	template<class T, class N, class Stack>
-	bool topIs(Stack&& s) {
-		return std::visit(compose(
-			[](T& type) {
 				return std::visit(compose(
-					[](N&) { return true; },
-					[](auto&&) { return false; }), type);
-			},
-			[](auto&&) { return false; }
-		), s.back());
+						[](G&) { return true; },
+						[](auto&&) { return false; }
+					), elem);
+			});
+	}
+
+	template<class G, class Iter>
+	Iter findFirst(Iter&& front, Iter&& back) {
+		return std::find_if(front, back, [](auto&& elem) {
+				return std::visit(compose(
+						[](G& g) {
+							return std::visit(compose(
+								[](T&) { return true; },
+								[](auto&&) { return false; }
+							), g);
+						},
+						[](auto&&) { return false; }
+					), elem);
+			});
+	}
+
+	/*
+	 * Return whether the top node is of a given type
+	 */
+	template<class G, class Stack>
+	bool at_top(Stack&& s) {
+		return std::holds_alternative<G>(s.back());
+	}
+
+	template<class G, class T, class Stack>
+	bool at_top(Stack&& s) {
+		auto group = std::holds_alternative<G>(s.back());
+		return group && std::holds_alternative<T>(std::get<G>(s.back()));
 	}
 }
