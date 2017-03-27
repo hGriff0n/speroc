7dab54ad6b266e76e6619af8198d196a576e66d9
hGriff0n <ghooper96@gmail.com>
2017-03-27 14:06:51 -0400

    Improved Test Displaying
    Improves the displaying of failed test cases and test group reports
    Adds in a more complete command line parser for specifying which test groups to run
    Adds in the ability to specify the yaml test file
    Changes tag to tags in testing yaml file to allow for more fine-grained "include/ignore" group controls

--- test.rb
+++ test.rb
@@ -1,54 +1,61 @@
 require 'yaml'
+require 'optparse'
 
 # TODO:
-#  Improve display of testing output
-#  Move test group naming to lowercase
-#    Implement more useful filtering of test cases (use 'tag' field)
+#  Switch over to the new yaml organization
+#    Make sure the new organization is covered
+#  Work on the displaying of the test cases (I don't quite like the look)
+#  Collect all output from the compilation and execution steps in an array
 
-# Catch output (of individual tests)
-# {testloc}: {status}:
-#   {test}
-# with {failure}
+# Parse out the command line arguments
+options = {
+    # provide default values for some commands
+    :file => './_test/tests.yaml',
+    :ignore => false,
+}
+OptionParser.new do |opt|
+    opt.on("-f", "--file [FILE]") { |o| options[:file] = o }
+    opt.on("-i", "--ignore") { |o| options[:ignore] = o }
+end.parse!
 
+ARGV.map! {|s| s.downcase}
 
-
-# TODO (from _todo.txt)
-# improve reporting/handling of tests in the ruby script
-# implement ability to selectively run tests from command line tags
-# work on improving the test yaml organization
-
-# Eventual TODO (Can't implement yet)
-#  Figure out how to report on compilation errors
-#    First need speroc to report compilation errors
-
+# Run the actual test framework
 test_num = 0
-YAML.load_file('./_test/tests.yaml').each do |name, tests|
-    if ARGV.size > 0 && !ARGV.include?(name) then
-        next
-    end
+YAML.load_file(options[:file]).each do |name, tests|
+    is_arg = (tests['tags'] & ARGV)
+    next if options[:ignore] && is_arg
+    next if !options[:ignore] && !is_arg
 
     # Test group setup
     num_correct = 0
-    puts "Running Test #{test_num}: #{name} - Testing #{tests['desc']}"
+    puts "Running Test Group \"#{name.capitalize}\""
+    puts "  - Testing #{tests['desc']}\n"
+    puts "======================================================="
 
     # Run all the tests in the test group
     tests['tests'].each do |test|
         # Collect information for calling the compiler from the test specification
         in_files = test['files'].map { |file| "./_test/#{file}" }.join(' ')
-        out_file = "_test/tmp/" + (test.key?('out') ? test['out'] : 'out.exe')
+        simp_out = test.key?('out') ? test['out'] : 'out.exe'
+        out_file = "_test/tmp/#{simp_out}"
         args = test['args'].join(' ')
+        simple_cmd = "speroc #{test['files'].join(' ')} -o #{simp_out} #{args}"
         speroc_cmd = "./_test/speroc #{in_files} -o #{out_file} #{args}"
 
 
         # Compile the program and catch any compilation errors
         IO.popen(speroc_cmd) do |io|
             # Capture any compilation errors here
-            io.read
+            compilation_out = io.readlines
             io.close
 
             # If compilation failed, display errors
             if $?.exitstatus != 0 then
-                puts "Test #{test_num} Failed:   Compilation Error"
+                puts " [#{test_num}]: #{simple_cmd}"
+                puts "    Compilation failed with errorstatus #{$?.exitstatus}"
+                # TODO: Report compilation messages
+                puts " --------------------------------------------------"
             end
         end
 
@@ -58,14 +65,17 @@ YAML.load_file('./_test/tests.yaml').each do |name, tests|
             IO.popen("./#{out_file}") do |io|
                 io.read
                 io.close
-            end
 
-            # Test whether it worked or not
-            if $?.exitstatus != test['value'] then
-                puts "Test #{test_num} Failed:   Expected value '#{test['value']}' - Got value '#{$?.to_i}'"
-            else
-                puts "Test #{test_num} Succeded with return '#{$?.exitstatus}'"
-                num_correct = num_correct + 1
+                # Test whether it worked or not
+                if $?.exitstatus != test['value'] then
+                    puts " [#{test_num}]: #{simple_cmd}"
+                    puts "    Execution failed with incorrect return value"
+                    puts "     Expected: #{test['value']}, Actual: #{$?.exitstatus}"
+                    puts " --------------------------------------------------"
+                else
+                    # puts "  Test #{test_num} Succeded with return '#{$?.exitstatus}'"
+                    num_correct = num_correct + 1
+                end
             end
         end
 
@@ -75,9 +85,18 @@ YAML.load_file('./_test/tests.yaml').each do |name, tests|
 
     # Report status of test programs
     num_tests = tests['tests'].size
-    num_failed = num_tests - num_correct
-    puts " -Report for Test Group #{name}: Succeed: #{num_correct}/#{num_tests} - Failed: #{num_failed}\n\n"
+    puts "  - Report for Test Group \"#{name.capitalize}\""
+    if num_tests != 0 then
+        puts "   #{num_tests - num_correct} casses out of #{num_tests} possible failed | #{num_correct / num_tests}% passed"
+    end
+    puts "=======================================================\n\n"
+    # puts " -Report for Test Group #{name}: Succeed: #{num_correct}/#{num_tests} - Failed: #{num_failed}\n\n"
 end
+
+# Catch output (of individual tests)
+# {testloc}: {status}:
+#   {test}
+# with {failure}
 
 =begin
 {test_group}
