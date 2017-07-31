65dc9a531d0a401db56daa7590ea54a718f2645c
Grayson Hooper <ghooper96@gmail.com>
2017-07-30 20:54:23 -0500

    [Update] Added Ability for "warn" Flag to Accept a Vector
    Slightly tweaks the semantics of the "warn" flag to allow for specifying multiple warnings/etc. (instead of forcing only one)

--- src/cmd_line.cpp
+++ src/cmd_line.cpp
@@ -1,5 +1,6 @@
 #include "cmd_line.h"
 #include <iostream>
+#include <vector>
 
 namespace spero {
 	namespace cmd {
@@ -18,7 +19,7 @@ namespace spero {
 				("C,stop", "Stop compilation at the assembly source file")
 				("diagnostics", "Specify a file to send diagnostic information to", value<std::string>())
 				("emit", "List of temporary files that the compiler should not delete", value<std::string>())
-				("W,warn", "Turn compilation warnings into errors", value<std::string>()->default_value("default"))
+				("W,warn", "Turn compilation warnings into errors", value<std::vector<std::string>>())
 				("L,showlog", "Display log messages along with warnings and errors")
 				("target", "Set the compilation target", value<std::string>()->default_value("win10"))
 				("o,out", "Specify output file", value<std::string>()->default_value("out.exe"));
